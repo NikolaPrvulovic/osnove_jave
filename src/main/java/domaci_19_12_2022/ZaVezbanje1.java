56ca3771caf3eac50a302515a427998f0e231ff7
NikolaPrvulovic <nikolaprvulovic@gmail.com>
2022-12-21 01:27:16 +0100

    Domaci - 19.12.2022. + tekst zadatka za vezbanje

--- src/main/java/domaci_19_12_2022/ZaVezbanje1.java
+++ src/main/java/domaci_19_12_2022/ZaVezbanje1.java
@@ -6,6 +6,15 @@ import java.util.Scanner;
 public class ZaVezbanje1 {
     public static void main(String[] args) {
 
+        //(Za vezbanje) Napisati program za ucenje matematike. Program daje korisniku 5 matematickih zadataka.
+        //Svaki nivo predstavlja jednu iteraciju petlje i u svakom nivou korisniku se prikazuju dva random broja koja treba da sabere i poruka o tacnosti resenja. (Program generise random brojeve u opsegu od 0 do 50, ne unosi ih korisnik)
+        //Random brojevi u Javi se generisisu kao u primeru
+        //// creating an object of Random class
+        //Random random = new Random();
+        //// Generates random integers 0 to 49
+        //int x = random.nextInt(50);
+        //Koristan link, koristan video
+
         Scanner s = new Scanner(System.in);
         Random random = new Random();
 
